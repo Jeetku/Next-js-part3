4ce6e64e6edfac57a99962124bdf985500018799
Jeetesh <jeeteshkumar45@gmail.com>
2023-08-11 12:02:15 +0530

    Get API using static db

---
+++ src/util/db.js
@@ -0,0 +1,32 @@
+export const user = [
+  {
+    id: 20,
+    name: "John",
+    age: 36,
+    email: "john@example.com",
+  },
+  {
+    id: 30,
+    name: "John",
+    age: 36,
+    email: "john@example.com",
+  },
+  {
+    id: 40,
+    name: "peter",
+    age: 32,
+    email: "peter@example.com",
+  },
+  {
+    id: 50,
+    name: "Harry",
+    age: 30,
+    email: "harry@example.com",
+  },
+  {
+    id: 60,
+    name: "Jeetesh",
+    age: 26,
+    email: "jeetesh@example.com",
+  },
+];
